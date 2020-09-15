7526adf6fe1c9551c39c2d6294fdbe3fecf00535
Aditya Borgaonkar <borg.aditya@gmail.com>
2020-09-15 21:40:56 +0530

    Assistant speaks back now!

--- assistant.py
+++ assistant.py
@@ -1,5 +1,10 @@
 import speech_recognition as sr
 import webbrowser
+import time
+import playsound
+import os
+import random
+from gtts import gTTS
 from time import ctime
 
 r = sr.Recognizer()
@@ -8,35 +13,49 @@ r = sr.Recognizer()
 def record_audio(ask=False):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            borg_speak(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
         except sr.UnknownValueError:
-            print('Sorry I didnt get that')
+            borg_speak('Sorry I didnt get that')
         except sr.RequestError:
-            print('Sorry my speech service is down')
+            borg_speak('Sorry my speech service is down')
         return voice_data
+
+
+def borg_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    r = random.randint(1, 1000000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
 
 
 def respond(voice_data):
     if 'what is your name' in voice_data:
-        print('my name is borg')
+        borg_speak('my name is borg')
     if 'what time is it' in voice_data:
-        print(ctime())
+        borg_speak(ctime())
     if 'search' in voice_data:
         search = record_audio('What do you want to search for')
         url = 'https://google.com/search?q=' + search
         webbrowser.get().open(url)
-        print('Here is what i found' + search)
+        borg_speak('Here is what i found' + search)
     if 'find location' in voice_data:
         location = record_audio('What is the location')
         url = 'https://google.nl/maps/place/' + location + '/&amp;'
         webbrowser.get().open(url)
-        print('Here is the location i found' + location)
+        borg_speak('Here is the location i found' + location)
+    if 'exit' in voice_data:
+        exit()
 
 
-print('How can I help you?')
-voice_data = record_audio()
-respond(voice_data)
+time.sleep(1)
+borg_speak('How can I help you?')
+while 1:
+    voice_data = record_audio()
+    respond(voice_data)
